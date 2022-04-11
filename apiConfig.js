e7f3f900177813245eaf1764cf69bc9823d81dd3
pspyoung <pspyoung2@gmail.com>
2022-04-11 09:01:06 -0700

    Adds node dependency

---
+++ apiConfig.js
@@ -0,0 +1,7 @@
+// put backend deployed link in falsey ternary option if you want to use dev API locally and prod API in deployment
+// const API_URL =
+// 	window.location.hostname === 'localhost' ? 'http://localhost:8000/' : '';
+
+const API_URL = 'https://pongscoresbackend.herokuapp.com/';
+
+export default API_URL;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const Players = require('./models/Players');
+// const Players = require('./models/Players');
 const port = process.env.PORT || 3000;
 
 // Middleware
