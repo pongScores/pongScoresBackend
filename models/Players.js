c76b88e9af4b2937b20780a0165158ec09034e38
pspyoung <pspyoung2@gmail.com>
2022-04-13 14:28:20 -0700

    Adds wins and losses to seedPlayers file and Players model.

--- models/Players.js
+++ models/Players.js
@@ -3,6 +3,8 @@ const mongoose = require('../db/connection');
 const PlayersSchema = new mongoose.Schema({
 	name: String,
 	rating: Number,
+	wins: Number,
+	losses: Number,
 });
 
 const Players = mongoose.model('Players', PlayersSchema);
